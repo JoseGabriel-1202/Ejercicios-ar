9725c2c9a8737929481f7473794452479012ecf9
JoseGabriel-1202 <78770273+JoseGabriel-1202@users.noreply.github.com>
2021-03-01 01:16:23 -0300

    Add files via upload

---
+++ 10mo programa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _10mo_programa
+{
+    class Program
+    {
+        
+        static void aNombre_y_edad()
+        {
+            string nombre;
+            int edad;
+            Console.WriteLine("ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingrese su edad");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hola " + nombre + "tu edad es: "+ edad);
+            if (edad >= 18) 
+            {
+                Console.WriteLine("usted es mayor de edad");
+            }
+        }
+        static void Main(string[] args)
+        {
+            aNombre_y_edad();
+        }
+
+    }
+}
