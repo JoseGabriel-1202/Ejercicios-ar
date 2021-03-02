9a38021ed8d8c2eae1c7616c1ded3687759ebbc2
JoseGabriel-1202 <78770273+JoseGabriel-1202@users.noreply.github.com>
2021-03-02 01:06:54 -0300

    Add files via upload

--- 1er programa/Program.cs
+++ 1er programa/Program.cs
@@ -6,18 +6,28 @@ namespace _1er_programa
     {
         static void Main(string[] args)
         {
-            int suma;
-            int[] numeros = new int[4];
+            double[] numeros = new double[4];
+            double suma = 0, media;
+            int num;
 
-            for (int i = 0; i < 4; i++) 
+            Console.Write("Ingrese 4 números para calcular su media" + "\n");
+
+            for (num = 0; num < 4; num++)
             {
-                Console.WriteLine("Digite un numero ");
-                numeros[i] = int.Parse(Console.ReadLine());
+                Console.Write("\n" + "Ingrese el valor {0}: ", num + 1);
+                numeros[num] = double.Parse(Console.ReadLine());
+                suma += numeros[num];
             }
-            suma = numeros[0] + numeros[1] + numeros[2] + numeros[3];
-            suma /= 4;
-            Console.WriteLine("la media es {0}: " , suma);
 
+            media = suma / 4;
+            Console.Write("\n" + "La media  es: {0}", media);
+
+            Console.Write("\n" + "estos son los valores ingresados: ");
+            for (num = 0; num < 4; num++)
+            {
+                Console.Write("\n" + "({0}) ", numeros[num]);
+            }
+            Console.ReadKey();
         }
     }
 }
